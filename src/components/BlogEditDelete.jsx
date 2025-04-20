0e91cd3bbe7093c1a2c6c41683d3cd0eeed5bd19
theresajvrensburg <theresa@isogramconsulting.com>
2025-04-20 13:32:54 +0200

    updates to BlogEditDelete and BlogPost components

--- src/components/BlogEditDelete.jsx
+++ src/components/BlogEditDelete.jsx
@@ -31,22 +31,14 @@ function BlogEditDelete(props) {
   const handleEdit = () => {
 
     setShow(!show);
-    // const requestBody = {
-    //   ...post,
-    // };
-
-    // axios
-    //   .put(`${API_URL}/api/posts/${id}`, requestBody)
-    //   .then(() => navigate(`/dashboard`))
-    //   .catch((error) => console.log(error));
   };
 
   const handleDelete = (e) => {
 
     e.preventDefault();
     
-    console.log("delete", id);
-    console.log(props);
+    //console.log("delete", id);
+    //console.log(props);
 
     axios
     .delete(`${API_URL}/api/posts/${id}`)
@@ -56,35 +48,43 @@ function BlogEditDelete(props) {
 
   return (
     <div key={time} className="flex flex-col blog-post">
-    <div>
-        <ProfileImage id={id} image={image} />
-
-        <div className="post-body">
-          <div className="top">
-            <User id={id} name={name} handle={handle} />
-            <Timestamp id={id} time={time} />
+      <div className="w-full p-4">
+        <div className="flex flex-row text-amber-950">
+          <ProfileImage id={id} image={image} />
+          <div>
+            <div className="flex flex-row">
+              <User id={id} name={name} handle={handle} />
+              <Timestamp id={id} time={time} />
+            </div>
+            <Message id={id} message={message} />
           </div>
+        </div>
+      </div>
 
-          <Message id={id} message={message} />
-          <Actions id={id} />
-          </div>
-    </div>
+      {show && (
+        <div className="flex items-center gap-x-1">
+          <BlogEdit id={id} message={message} />
+        </div>
+      )}
 
-    {show && <div className="flex items-center gap-x-1">
-        <BlogEdit id={id} message={message} />
-      </div>}
-
-      <div className="flex items-center gap-x-1">
-        <Button variant="text" size="sm" className="hidden lg:inline-block" onClick={handleEdit}>
+      <div className="flex justify-center gap-x-1">
+        <Button
+          variant="text"
+          size="sm"
+          className="hidden lg:inline-block"
+          onClick={handleEdit}
+        >
           <span> Edit </span>
         </Button>
-        <Button variant="gradient" size="sm" className="hidden lg:inline-block" onClick={handleDelete}>
+        <Button
+          variant="gradient"
+          size="sm"
+          className="hidden lg:inline-block"
+          onClick={handleDelete}
+        >
           <span> Delete </span>
         </Button>
       </div>
-
-     
-
     </div>
   );
 }
--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -7,19 +7,19 @@ function BlogPost(props) {
   const {id, image, name, handle, time, message} = props;
   return (
     <div key={time} className="blog-post">
-      <ProfileImage id={id} image={image}/>
-      
-      <div className="post-body">
-        <div className="top">
-          <User id={id} name={name} handle={handle}/>
-          <Timestamp id={id} time={time}/>
+      <div className="w-full p-4">
+        <div className="flex flex-row">
+          <ProfileImage id={id} image={image} />
+          <div className='text-amber-950'>
+            <div className="flex flex-row text-amber-950">
+              <User id={id} name={name} handle={handle} />
+              <Timestamp id={id} time={time} />
+            </div>
+            <Message id={id} message={message} />
+          </div>
         </div>
-
-       <Message id={id} message={message}/>
-       <Actions id={id}/>
       </div>
 
-      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
